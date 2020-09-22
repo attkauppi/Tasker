01e381e25acfcf8ef791744f17860442438dfb72
Ari Kauppi <ari.kauppi@santelo.fi>
2020-09-22 09:18:57 +0300

    Replaced Procfile settings to use wsgi:app instead of application:app as example suggested. Works at least locally

--- application/schema.sql
+++ application/schema.sql
@@ -1,15 +1,4 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT
-);
-
-CREATE TABLE messages (
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    user_id INTEGER REFERENCES users,
-    sent_at TIMESTAMP
-);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE messages (id SERIAL PRIMARY KEY, content TEXT);
